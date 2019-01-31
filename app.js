968bf3575902ea0545b1f70811530d3b393114af
tiffany <tiffany831101@gmail.com>
2019-01-31 22:44:13 +0800

    changeport

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express();
 app.use('/static', express.static('public'));
 
 //監聽端口
-app.listen(3001, function () {
+app.listen(3002, function () {
     console.log("server listen at port 3001")
 })
